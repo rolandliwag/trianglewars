2ca2636041e4468a4acc762dbca2311346d699f1
JR Liwag <rl@one.com>
2014-10-11 12:47:44 +0400

    Game loop and some styling of main stage.

--- http-pub/lib/Application.js
+++ http-pub/lib/Application.js
@@ -1,8 +1,30 @@
 function Application() {
+    var app = this;
+
+    this.entities = [];
+
     // Initialize modules/models
 
+
     // Start the game loop
+    this.lastFrameTime = Date.now();
+    this.gameLoop = setInterval(function () {
+        app.run();
+        app.lastFrameTime = Date.now();
+    }, 1000 / 60); // 60fps
 }
+
+Application.prototype.run = function () {
+    var timeNow = Date.now();
+
+    this.entities.forEach(function (entity) {
+        entity.draw(timeNow);
+    });
+};
+
+Application.prototype.end = function () {
+    clearInterval(this.gameLoop);
+};
 
 $(function () {
     var app = new Application();
